468d0fac12096643f0d414e73bc26b5ed1a6aafb
Micah Silverman <micah@afitnerd.com>
2020-08-18 19:03:46 -0400

    baseline organization

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <OktaCliDocs/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OktaCliDocs from '@/components/OktaCliDocs.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    OktaCliDocs
   }
 }
 </script>
---
+++ src/components/OktaCliDocs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h2>{{ platform }}</h2>
+  </div>
+</template>
+
+<script>
+
+import os from 'platform-detect/os.mjs'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    platform() {
+      let _os = 'UNKNOWN';
+      if (os.windows) {
+        _os = 'windows';
+      } else if (os.linux) {
+        _os = 'linux';
+      } else if (os.macos) {
+        _os = 'macos';
+      }
+      return _os;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
