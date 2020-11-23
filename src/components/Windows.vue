70f28ec72a735022c17552de6de9be7197dc526b
Brian Demers <bdemers@apache.org>
2020-11-23 12:00:41 -0500

    Update instructions for 0.7.1

--- src/components/Windows.vue
+++ src/components/Windows.vue
@@ -15,7 +15,7 @@ export default {
                 @"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin"
 
                 rem Then:
-                choco install okta --version 0.7.0`,
+                choco install okta --version 0.7.1`,
             cmOptions: {
                 mode: 'text/x-sh',
                 lineNumbers: true,
